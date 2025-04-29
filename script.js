d92b709871531fb98286ecdedf0b4bf4fec26ab7
KPRMODI <kpmodi@my.waketech.edu>
2025-04-29 14:01:27 -0400

    added

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-let hi = document.getElementById("l")
+// The defining of variables that will be used all throughout the code
 
-let tasks = 
+let submit = document.getElementById("submit")
+let taskpriority = document.getElementById("taskpriority")
+let taskmanager = document.getElementById("taskmanager")
+let p = document.createElement("p")
 
-hi.addEventListener("click", () =>{
+let tasks = {
+    id: null,
+    name: null,
+    priority: null,
+    important: null,
+    date: null,
+}
+
+task.task = task.value
+
+
+submit.addEventListener("click", () =>{
     console.log("Hi")
+    p.innerHTML = "hi"
+    taskmanager.appendChild(p)
+    let new1 = document.createElement("input type=`checkbox`")
+    taskmanager.appendChild(new1)
+
 })
+
+console.log()
