eafbed33224c93b7f6390a6827fcef2831204374
KPRMODI <kpmodi@my.waketech.edu>
2025-05-01 13:40:41 -0400

    added

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # WEB-115-FinalProject-Modi
+
+
+
+Approach:
+
+I took this problem on one step at a time, working on one part of the code at a time and making sure that code worked before moving on. With this logic, I first worked on the HTML buttons and ids to later define in my JS file. After doing that, I worked on an event listener whenver the submit button was clicked so that it could perform everything. I then added checking to see if the task was empty or not.
+
+I then made the object using the ID element values, and proceesed to console log the task object so the user could see the values. Afterwards, I spent a lot of time working on how the task would be displayed on the page. I decided to use a div approach, created a new div to append as a child to the original div I created and then append sub children to the new div which would all be contained within the new div. This worked for me and then I just inputted all of the user data into the new div to be displayed onto the screen. I then added formatting and CSS and that was the final part of the problem I completed.
+
+
+Issues:
+
+While working on this project I have encountered several issues which some I was able to fix, and some that I couldn't fix or just didn't have enough time to do so.
+
+The ones that I was able to fix were the ones that took me the longest to solve, such as not remembering how to create a checkbox or the drop down buttons for the HTML buttons.
+I also had problem getting true and false from the check box and had to remember to use .checked to get true and false from it, and this also took me a long time to figure out which 
+disrupted my console.logs and statement handling later on.
+
+Some Problems I still have with this is dynamically changing the console log to match the "isCompleted" key in the tasks object, and I didn't know how to solve this issue. To fix this issue the best I could, I would just use another consolelog statement to tell the user that the isCompleted key is now true.
+
+Another issue I had was style formatting the undo button for the task complete checkbox. I didn't know how to only remove the line-through CSS for it, so I had to removeattribute the styles and CSS which removed all of the formatting, and I haven't been able to find a fix to this. 
--- script.js
+++ script.js
@@ -1,78 +1,78 @@
 // The defining of variables that will be used all throughout the code
-
 let submit = document.getElementById("submit")
 let taskpriority = document.getElementById("taskpriority")
 let taskmanager = document.getElementById("taskmanager")
 let importantcheckbox = document.getElementById("important")
-let id = 1
+let id = 1 
 let input = document.getElementById("input")
-
-let ten = []
-
-
 let tasks = { } // Defines the tasks object, which will add ids and keys when the user submits the task
 
-let date = Date()
-
 submit.addEventListener("click", () =>{ // When submit is clicked, assign all values to the object 
-     if (input.value === "") {
-    window.alert("You must enter something")
-} else{
-    console.log(important.value)
+     if (input.value === "") { // checks if input is blank, and if it isint go to else statement
+        window.alert("You must enter something")
+    } else {
+    // defines tasks object keys and id's
     tasks.id = id
     tasks.input = input.value
     tasks.priority = taskpriority.value
-    tasks.isImportant = importantcheckbox.value
+    tasks.isImportant = importantcheckbox.checked
     tasks.isCompleted = false
-    tasks.date = date.value
+    tasks.date = Date().slice(0,10)
 
-
-
-    console.log(tasks)
+    // shows console log tasks object in JSON string format
+    console.log(JSON.stringify(tasks))
 
     let newdiv = document.createElement("div")
-    taskmanager.appendChild(newdiv)
+    taskmanager.appendChild(newdiv) // creates a new div to append
 
-    ten.push(newdiv)
-
+    // CSS formatting for the boxes
     newdiv.style.border = "2px solid"
-    newdiv.style.padding = "20px"
-    newdiv.style.margin = "20px"
+    newdiv.style.padding = "10px"
+    newdiv.style.margin = "10px"
     newdiv.style.fontSize = "20px"
-    if (taskpriority.value === "high") {
-        newdiv.style.color = "red"
+
+    // if important checkbox is clicked then turn background color of box red
+    if (importantcheckbox.checked == true) {
+        newdiv.style.backgroundColor = "red"
     }
-
-
-
-
-
+ 
+    // Creates a task data element that gets all values and appends it to the new div element as a child
     let taskdata = document.createElement("p")
-    taskdata.innerHTML = `Task: ${input.value} <br> priority: ${taskpriority.value} <br> date: ${Date()}`
+    taskdata.innerHTML = `Task: ${input.value} <br> priority: ${taskpriority.value} <br> date: ${Date().slice(0,10)}`
     newdiv.appendChild(taskdata)
 
-        let newcheck = document.createElement(`input`)
-        newcheck.setAttribute("type", "checkbox")
-        newdiv.appendChild(newcheck)
+    // Created for the check box for completion of task
+    let newcheck = document.createElement(`input`)
+    newcheck.setAttribute("type", "checkbox")
+    newdiv.appendChild(newcheck)
 
-        checkname = document.createElement("p")
-        checkname.innerHTML = "done"
-        newdiv.appendChild(checkname)
+    // Text Indicator of check  box
+    checkname = document.createElement("p")
+    checkname.innerHTML = "Click checkbox if done"
+    newdiv.appendChild(checkname)
 
+    // creation of the deletebutton that deletes the task
+    let deletebutton = document.createElement("button")
+    deletebutton.innerHTML = "Press to Delete task"
+    newdiv.appendChild(deletebutton)
 
-    let i = document.createElement("button")
-    i.innerHTML = "Delete"
-    newdiv.appendChild(i)
-
-    let hi = document.getElementById("hi")
-
-    i.addEventListener("click", () =>{
+    // Event listener that removes child if clicked
+    deletebutton.addEventListener("click", () =>{
         taskmanager.removeChild(newdiv)
     })
 
+    // Add a line through if the check box is checked
+    newcheck.addEventListener("click", () =>{
+        newdiv.style.textDecoration = "line-through"
+        tasks.isCompleted = true
+        checkname.innerHTML = "undo"
+        console.log(JSON.stringify(tasks))
+        newcheck.addEventListener("click", () =>{ // is supposed to undo this, but I couldn't get it to work
+            newdiv.removeAttribute("style","textDecoration: line-through")
+    
+        })
+})
 
     id += 1 // for each time the submit button is pressed, just do this
     }
 })
-
-console.log()
